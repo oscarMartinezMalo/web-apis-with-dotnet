ec5c848fded5cb620065f78bfa9a241c07a2f396
Oscar M <ommalor@gmail.com>
2024-06-24 11:51:44 -0400

    Creating Api

---
+++ src/SoftwareCatalogSolution/SoftwareCatalog.Api/Api.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SoftwareCatalog.Api;
+
+public class Api : ControllerBase
+{
+
+    [HttpPost("/techs")]
+    public async Task<ActionResult> AddATechAsync(
+        [FromBody] CreateTechRequest request,
+        CancellationToken token)
+    {
+        return Ok();
+    }
+}
+
+public record CreateTechRequest
+{
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Phone { get; set; } = string.Empty;
+}
